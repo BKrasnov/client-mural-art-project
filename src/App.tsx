10fc1c97d7d24825586d4e034512a82ee6c2bec8
bogdan <bogdan.krasnov@saritasa.com>
2022-09-08 23:06:55 +0700

    Setup react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { FC, Suspense } from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+import { store } from './core/store';
+import { RootRouter } from './core/routes/RootRouter';
 
-export default App;
+import './theme/index.css';
+
+/** Replace "Brr... gere should..." on "<ErrorSite>" */
+export const App: FC = () => (
+  <Provider store={store}>
+    <HashRouter>
+      <div>
+        <Suspense fallback={<div>Brrr... here should be your loader component</div>}>
+          <RootRouter />
+        </Suspense>
+      </div>
+    </HashRouter>
+  </Provider>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+import { App } from './App';
+
+import '../src/theme/index.css';
+
+const rootElement: HTMLElement | null = document.getElementById('root');
+if (rootElement === null) {
+  throw new Error('Failed to find root element');
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
 
