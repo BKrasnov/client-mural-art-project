65ddbe31b992db4d6a194b6ec12e6f4fb1326533
bogdan <bogdan.krasnov@saritasa.com>
2022-09-08 23:07:08 +0700

    Create Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { FC, memo, useState } from "react";
+
+import "./Footer.css";
+
+
+const FooterComponent: FC = () => {
+
+  return (
+    <>
+      Footer
+    </>
+  );
+};
+
+export const Footer = memo(FooterComponent);
