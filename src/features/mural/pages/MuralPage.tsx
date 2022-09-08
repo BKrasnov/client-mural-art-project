1127411fa33c052f1450f50c86e5443feb1fc051
bogdan <bogdan.krasnov@saritasa.com>
2022-09-08 23:06:09 +0700

    Create Mural components

---
+++ src/features/mural/pages/MuralPage.tsx
@@ -0,0 +1,12 @@
+import { FC, memo } from 'react';
+
+
+// import styles from './AnimePage.module.css';
+
+const MuralPageComponent: FC = () => (
+  <>
+    <div>Hello mural</div>
+  </>
+);
+
+export const MuralPage = memo(MuralPageComponent);
