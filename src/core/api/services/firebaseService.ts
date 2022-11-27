17ff005c5c27f916386f3feed2ddbdf4e6d0e82e
BKrasnov <bkrasnov235@gmail.com>
2022-11-27 19:40:01 +0700

    Update firebaseService

--- src/core/api/services/firebaseService.ts
+++ src/core/api/services/firebaseService.ts
@@ -11,10 +11,10 @@ export namespace FirebaseService {
   const app = initializeApp(firebaseConfig);
 
   /**  Create a db. */
-  export const db = getFirestore(app);
+  export const database = getFirestore(app);
 
-  /** Collection of books from the Firebase. */
-  export const muralCollectionRef = collection(db, MURAL_NAME_COLLECTION);
+  /** Collection of murals from the Firebase. */
+  export const muralCollectionRef = collection(database, MURAL_NAME_COLLECTION);
 
   export const auth = getAuth(app);
 }
