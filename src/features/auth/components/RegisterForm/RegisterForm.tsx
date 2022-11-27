c96716c4eb203b527c170df8200da730dc615961
BKrasnov <bkrasnov235@gmail.com>
2022-11-27 22:32:16 +0700

    Create RegisterForm

--- src/features/auth/components/RegisterForm/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm/RegisterForm.tsx
@@ -1,9 +1,55 @@
-import { FC, memo } from "react";
+import { ChangeEvent, FC, memo, useState } from "react";
 
-const RegistrarionFormComponent: FC = () => (
-  <>
-    <div>Register</div>
-  </>
-);
+import { useAppDispatch } from "@core/store";
+import { authRegister } from "@core/store/auth/dispatchers";
+
+import { Registration } from "@core/models";
+
+const INITIAL_USER: Registration = {
+  email: '',
+  password: '',
+};
+
+const RegistrarionFormComponent: FC = () => {
+  const [userData, setUserData] = useState(INITIAL_USER);
+
+  /**
+   * Handle input change.
+   * @todo Fix code style.
+   * @param event
+   */
+  const handleInputs = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputs = { [event.target.name]: event.target.value };
+
+    setUserData({ ...userData, ...inputs });
+  };
+
+  const dispatch = useAppDispatch();
+
+  // const isLoading = useAppSelector(selectUserLoading);
+  // const registrationError = useAppSelector(selectUserError);
+
+  const handleUserRegister = (registerData: Registration) => {
+    dispatch(authRegister(registerData));
+  };
+
+  return (
+    <>
+      Registrarion
+      <input placeholder="Email" name="email" type="email" onChange={event => handleInputs(event)} />
+      <input placeholder="Password" name="password" type="password" onChange={event => handleInputs(event)} />
+      <button
+        onClick={() => {
+          handleUserRegister({
+            email: userData.email,
+            password: userData.password,
+          });
+        }}
+      >
+        register
+      </button>
+    </>
+  );
+};
 
 export const RegistrarionForm = memo(RegistrarionFormComponent);
