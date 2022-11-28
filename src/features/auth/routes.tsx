1285e9130e0156878af92aa2f62fb7ba41392aad
BKrasnov <bkrasnov235@gmail.com>
2022-11-28 21:02:55 +0700

    Added NonAuthGuard in auth routes

--- src/features/auth/routes.tsx
+++ src/features/auth/routes.tsx
@@ -1,27 +1,34 @@
 import { lazy } from "react";
 import { Navigate, RouteObject } from "react-router-dom";
-import { LoginForm } from "./components/LoginForm";
 
+import { NonAuthGuard } from "@core/routes/guards/NonAuthGuard";
+
+import { LoginForm } from "./components/LoginForm";
 import { RegistrarionForm } from "./components/RegisterForm/RegisterForm";
 
-const AuthPage = lazy(() => import("./pages/AuthPage").then((module) => ({ default: module.AuthPage })));
+const AuthPage = lazy(() => import("./pages/AuthPage").then(module => ({ default: module.AuthPage })));
 
 export const authRoutes: RouteObject[] = [
   {
-    path: "auth",
-    element: <AuthPage />,
+    element: <NonAuthGuard />,
     children: [
       {
-        path: "registration",
-        element: <RegistrarionForm />,
-      },
-      {
-        path: "login",
-        element: <LoginForm />,
-      },
-      {
-        path: "*",
-        element: <Navigate to="/auth/login" />,
+        path: "auth",
+        element: <AuthPage />,
+        children: [
+          {
+            path: "registration",
+            element: <RegistrarionForm />,
+          },
+          {
+            path: "login",
+            element: <LoginForm />,
+          },
+          {
+            path: "*",
+            element: <Navigate to="/auth/login" />,
+          },
+        ],
       },
     ],
   },
