5a83d5c0d26ff233af83a3413032400a77a4a1d9
BKrasnov <bkrasnov235@gmail.com>
2022-11-28 20:56:58 +0700

    Created function getUser in UserService

--- src/core/api/services/userService.ts
+++ src/core/api/services/userService.ts
@@ -1,12 +1,12 @@
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { addDoc } from "firebase/firestore/lite";
 
 import { FirebaseService } from "./firebaseService";
 
 import { User } from "@core/models";
+import { UserMapper } from "../mappers";
 
 export namespace UserService {
-
   /**
    * Adding a user to the collection.
    * @param user The user object to be stored.
@@ -14,17 +14,34 @@ export namespace UserService {
    */
   export const addUser = async (user: User): Promise<void> => {
     try {
-      const userRef = await addDoc(FirebaseService.userCollectionRef, user);
-      console.log("Document written with ID: ", userRef.id);
+      await addDoc(FirebaseService.userCollectionRef, user);
     } catch (error) {
       console.error("Error adding document: ", error);
     }
   };
+
+  /** Function which tries to get a user from cache. */
+  export function getUser(): Promise<User | null> {
+    return new Promise((resolve, reject) => {
+      const unsubscribe = onAuthStateChanged(
+        FirebaseService.auth,
+        user => {
+          unsubscribe();
+          resolve(user !== null ? UserMapper.fromUserDto(user) : user);
+        },
+        error => {
+          unsubscribe();
+          reject(error);
+        }
+      );
+    });
+  }
 
   /**
    * Logout a user.
    */
   export async function logout(): Promise<void> {
     await signOut(FirebaseService.auth);
+    console.log("User logged out");
   }
 }
