e155d29091db1058ba279f461f7eca3131070f9b
BKrasnov <bkrasnov235@gmail.com>
2022-12-02 23:03:22 +0700

    Added handleSubmitUserLogout in SideBar

--- src/features/personal-area/components/Sidebar/Sidebar.tsx
+++ src/features/personal-area/components/Sidebar/Sidebar.tsx
@@ -3,6 +3,8 @@ import { FC, memo } from "react";
 import styles from "./Sidebar.module.css";
 
 import logotype from "@static/logotype.png";
+import { authLogout } from "@core/store/auth/dispatchers";
+import { useAppDispatch } from "@core/store";
 
 const menuItem = [
   {
@@ -21,29 +23,43 @@ const menuItem = [
     name: "Настройки",
     address: "/",
   },
-  {
-    name: "Выход",
-    address: "/",
-  },
 ];
 
-const SidebarComponent: FC = () => (
-  <div className={styles.sidebar}>
-    <div><img className={styles.logo} src={logotype} alt="" /></div>
-    <ul className={styles.sidebar__menu}>
-      {menuItem.map((item) => {
-        return (
-          <li className={styles.sidebar__link}>
-            <a href={item.address}>
-              <button className={styles.sidebar__button}>
-                <span>{item.name}</span>
-              </button>
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-);
+const SidebarComponent: FC = () => {
+  const dispatch = useAppDispatch();
+
+  /** User logout */
+  const handleSubmitUserLogout = () => {
+    dispatch(authLogout());
+  };
+
+  return (
+    <div className={styles.sidebar}>
+      <div>
+        <img className={styles.logo} src={logotype} alt="" />
+      </div>
+      <ul className={styles.sidebar__menu}>
+        {menuItem.map(item => {
+          return (
+            <li className={styles.sidebar__link}>
+              <a href={item.address}>
+                <button className={styles.sidebar__button}>
+                  <span>{item.name}</span>
+                </button>
+              </a>
+            </li>
+          );
+        })}
+        <li className={styles.sidebar__link}>
+          <a href="/">
+            <button onClick={handleSubmitUserLogout} className={styles.sidebar__button}>
+              <span>Выход</span>
+            </button>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export const Sidebar = memo(SidebarComponent);
