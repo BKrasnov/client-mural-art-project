559209fee9a16c5e1b77ae4e365a60da65f33b82
BKrasnov <bkrasnov235@gmail.com>
2022-12-04 14:54:43 +0700

    Update authStateProvider

--- src/components/authStateProvider/authStateProvider.tsx
+++ src/components/authStateProvider/authStateProvider.tsx
@@ -2,15 +2,23 @@ import { memo, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 
+import { AppDispatch, useAppSelector } from "@core/store";
 import { getUserFromCache } from "@core/store/auth/dispatchers";
-import { AppDispatch } from "@core/store";
+import { selectIsAuthLoading } from "@core/store/auth/selectors";
 
 const AuthStateProviderComponent = () => {
   const dispatch = useDispatch<AppDispatch>();
-
+  const isLoading = useAppSelector(selectIsAuthLoading);
+  
   useEffect(() => {
     dispatch(getUserFromCache());
-  });
+  }, [dispatch]);
+
+  if(isLoading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
 
   return <Outlet />;
 };
