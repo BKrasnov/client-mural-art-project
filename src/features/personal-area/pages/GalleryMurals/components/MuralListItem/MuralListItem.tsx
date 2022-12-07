ee5740b56ec9dcb3fc0cd2a87545e215480e4638
BKrasnov <bkrasnov235@gmail.com>
2022-12-07 15:59:50 +0700

    Added Link component from react-router

--- src/features/personal-area/pages/GalleryMurals/components/MuralListItem/MuralListItem.tsx
+++ src/features/personal-area/pages/GalleryMurals/components/MuralListItem/MuralListItem.tsx
@@ -1,27 +1,32 @@
 import { FC, memo } from "react";
+import { Link } from "react-router-dom";
 
 import { Mural } from "@core/models";
 
 import styles from "./MuralListItem.module.css";
 
 interface Props {
-  /** Mural. */
+  /** MuralList element. */
   readonly mural: Mural;
 }
 
 const MuralListItemComponent: FC<Props> = ({ mural }) => {
+  const linkToMuralId = `mural/${mural.id}`;
 
   return (
-    <button className={styles.mural}>
-      <div className={styles.mural__imageContainer}>
-        <img className={styles.mural__image} src={mural.imageRef} alt="mural" />
-      </div>
-      <div className={styles.mural__info}>
-        <h1 className={styles.mural__title}>{mural.title}</h1>
-        <span className={styles.mural__city}>{mural.location}</span>
-        <span className={styles.mural__event}>Street-art Красноярск</span>
-      </div>
-    </button>
+    <>
+      <button className={styles.mural}>
+        <Link to={linkToMuralId}>
+          <div className={styles.mural__imageContainer}>
+            <img className={styles.mural__image} src={mural.imageRef} alt="mural" />
+          </div>
+          <div className={styles.mural__info}>
+            <h1 className={styles.mural__title}>{mural.title}</h1>
+            <span className={styles.mural__city}>{mural.location ?? <span>Нет города</span>}</span>
+          </div>
+        </Link>
+      </button>
+    </>
   );
 };
 
