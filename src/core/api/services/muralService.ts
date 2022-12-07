25df6ccbba63256542cc0fa10a20bb263157b26b
BKrasnov <bkrasnov235@gmail.com>
2022-12-07 15:52:49 +0700

    Create getMuralById function

--- src/core/api/services/muralService.ts
+++ src/core/api/services/muralService.ts
@@ -1,4 +1,4 @@
-import { getDocs, query } from "firebase/firestore/lite";
+import { getDocs, limit, query, where } from "firebase/firestore/lite";
 import { FirebaseService } from "./firebaseService";
 
 import { MuralMapper } from "@core/api/mappers";
@@ -11,10 +11,26 @@ export namespace MuralService {
 
   const muralCollection = FirebaseService.getCollectionReference<MuralDto>(MURAL_NAME_COLLECTION);
 
+  /** Getting all murals from firebase. */
   export async function getMural(): Promise<Mural[]> {
     const queryMurals = query(muralCollection);
-    const querySnapshot = await getDocs(queryMurals);
-    const murals: Mural[] = FirebaseService.mapQuerySnapshotToArray<MuralDto, Mural>(querySnapshot, MuralMapper.fromDto);
+    const muralsSnapshot = await getDocs(queryMurals);
+    const murals: Mural[] = FirebaseService.mapQuerySnapshotToArray<MuralDto, Mural>(muralsSnapshot, MuralMapper.fromDto);
     return murals;
+  }
+
+  /**
+   * Getting mural by id from firebase.
+   * @param id Mural id.
+   * @throws Error in case mural with provided id doesn't exist.
+   */
+  export async function getMuralById(id: number): Promise<Mural> {
+    const queryMural = query(muralCollection, where("id", "==", id), limit(1));
+    const muralSnapshot = await getDocs(queryMural);
+    if (muralSnapshot.empty) {
+      throw new Error(`Mural with id ${id} doesn't exist`);
+    }
+    
+    return MuralMapper.fromDto(muralSnapshot.docs[0].data());
   }
 }
