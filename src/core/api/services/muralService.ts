b06436c4ff40c1c2c88926b5cb5b944ef77a7e9e
BKrasnov <bkrasnov235@gmail.com>
2022-12-08 13:58:41 +0700

    Update getMuralsQueryConstraints

--- src/core/api/services/muralService.ts
+++ src/core/api/services/muralService.ts
@@ -1,4 +1,4 @@
-import { getDocs, limit, query, QueryConstraint, where } from "firebase/firestore/lite";
+import { endAt, getDocs, limit, orderBy, query, QueryConstraint, startAt, where } from "firebase/firestore/lite";
 
 import { FirebaseService } from "./firebaseService";
 
@@ -14,8 +14,6 @@ import { Mural, MuralsFetchingOptions } from "@core/models/murals";
 function getMuralsQueryConstraints(options: MuralsFetchingOptions): QueryConstraint[] {
   const constraints: QueryConstraint[] = [];
 
-  /** Fetching +1 to know if there is next page. */
-  constraints.push(limit(options.countOfMurals + 1));
   const { searchValue } = options.filters;
 
   if (searchValue.trim() !== "") {
@@ -24,8 +22,11 @@ function getMuralsQueryConstraints(options: MuralsFetchingOptions): QueryConstra
        Because it is after most regular characters in Unicode,
        the query matches all values that start with searching value. */
     const veryHighCodePoint = "\uf8ff";
-    constraints.push(where("title", ">=", searchValue));
-    constraints.push(where("title", "<=", `${searchValue}${veryHighCodePoint}`));
+
+    constraints.push(orderBy("title"));
+    constraints.push(startAt(searchValue.toLowerCase()));
+    constraints.push(endAt(`${searchValue}${veryHighCodePoint}`));
+    console.log(constraints);
   }
   return constraints;
 }
@@ -33,14 +34,15 @@ function getMuralsQueryConstraints(options: MuralsFetchingOptions): QueryConstra
 export namespace MuralService {
   const MURAL_NAME_COLLECTION = "murals";
 
-  const muralCollection = FirebaseService.getCollectionReference<MuralDto>(MURAL_NAME_COLLECTION);
+  const muralCollectionRef = FirebaseService.getCollectionReference<MuralDto>(MURAL_NAME_COLLECTION);
 
-  /** 
+  /**
    * Getting all murals from firebase.
    * @param options Options for fetching murals.
    */
   export async function getMurals(options: MuralsFetchingOptions): Promise<Mural[]> {
-    const queryMurals = query(muralCollection, ...getMuralsQueryConstraints(options));
+    /** @todo ...getMuralsQueryConstraints(options) */
+    const queryMurals = query(muralCollectionRef);
     const muralsSnapshot = await getDocs(queryMurals);
     const murals: Mural[] = FirebaseService.mapQuerySnapshotToArray<MuralDto, Mural>(muralsSnapshot, MuralMapper.fromDto);
     return murals;
@@ -52,7 +54,7 @@ export namespace MuralService {
    * @throws Error in case mural with provided id doesn't exist.
    */
   export async function getMuralById(id: number): Promise<Mural> {
-    const queryMural = query(muralCollection, where("id", "==", id), limit(1));
+    const queryMural = query(muralCollectionRef, where("id", "==", id), limit(1));
     const muralSnapshot = await getDocs(queryMural);
     if (muralSnapshot.empty) {
       throw new Error(`Mural with id ${id} doesn't exist`);
