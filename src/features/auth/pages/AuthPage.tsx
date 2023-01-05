8324aa1363343889f906ebb975adb9518dddd2f3
BKrasnov <bkrasnov235@gmail.com>
2023-01-05 13:12:46 +0700

    Create HeaderForm M-1

--- src/features/auth/pages/AuthPage.tsx
+++ src/features/auth/pages/AuthPage.tsx
@@ -1,14 +1,18 @@
 import { FC, memo } from "react";
 import { Outlet } from "react-router-dom";
+import { HeaderForm } from "../components/HeaderForm";
 
 import styles from "./AuthPage.module.css";
 
 export const AuthPageComponent: FC = () => (
-  <div className={styles.container}>
-    <div className={styles.wrapperForm}>
-      <Outlet />
+  <>
+    <HeaderForm/>
+    <div className={styles.container}>
+      <div className={styles.wrapperForm}>
+        <Outlet />
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export const AuthPage = memo(AuthPageComponent);
