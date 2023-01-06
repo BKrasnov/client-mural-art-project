fcf7a89c462e6493798cfc9031f3c8b5f502d33c
BKrasnov <bkrasnov235@gmail.com>
2023-01-06 21:07:41 +0700

    Add isLoadingUser ad user in NonAuthGuard M-1

--- src/routes/guards/NonAuthGuard.tsx
+++ src/routes/guards/NonAuthGuard.tsx
@@ -2,12 +2,20 @@ import { memo } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 import { useAppSelector } from "src/store";
-import { selectIsAuth } from "src/store/auth/selectors";
+import { selectIsAuth, selectUser } from "src/store/auth/selectors";
+import { selectIsLoadingUser } from "src/store/user/selectors";
 
 const NonAuthGuardComponent = () => {
-  const isAuthUser = useAppSelector(selectIsAuth);
-  if (isAuthUser) {
-    return <Navigate to="/personal-area/profile" />;
+  const isSignedIn = useAppSelector(selectIsAuth);
+  const isLoadingUser = useAppSelector(selectIsLoadingUser)
+  const user = useAppSelector(selectUser);
+
+  if(user?.firstName === null && user.lastName === null) {
+    return <Navigate to="/auth/profile" />;
+  } else {
+    if (isSignedIn && !isLoadingUser) {
+      return <Navigate to="/personal-area/profile" />;
+    }
   }
   return <Outlet />;
 };
