6515a3101e380f5c65366636c81ff9416e93d7fc
BKrasnov <bkrasnov235@gmail.com>
2023-01-08 01:25:12 +0700

    Create OptionsSelect M-1

---
+++ src/core/models/auth/optionsSelect.ts
@@ -0,0 +1,6 @@
+/** Options for the select tag. */
+export interface OptionsSelect {
+  key: string;
+  value: string;
+  disabled?: boolean;
+}
--- src/core/models/index.ts
+++ src/core/models/index.ts
@@ -3,6 +3,7 @@ export * from "./auth/login";
 export * from "./auth/registration";
 export * from "./auth/token";
 export * from "./auth/profile";
+export * from "./auth/optionsSelect";
 
 export * from "./errors/app-error";
 export * from "./errors/form-error";
