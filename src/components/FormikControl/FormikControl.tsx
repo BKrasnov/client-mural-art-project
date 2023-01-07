bf9371e940fe051fb022380204a6bd9c382e2a1c
BKrasnov <bkrasnov235@gmail.com>
2023-01-08 01:22:13 +0700

    Create FormikControl M-1

---
+++ src/components/FormikControl/FormikControl.tsx
@@ -0,0 +1,51 @@
+import { FC } from "react";
+
+import { FieldHookConfig, useField, ErrorMessage } from "formik";
+
+import { Input, Select } from "./components/";
+
+import { OptionsSelect } from "@core/models";
+
+import styles from "./FormikControl.module.css";
+
+interface FieldSettings {
+  control: string;
+  options?: OptionsSelect[];
+}
+
+export const FormikControl: FC<FieldHookConfig<string> & FieldSettings> = ({
+  control,
+  options,
+  ...props
+}) => {
+  const [field] = useField(props);
+
+  switch (control) {
+    case "input":
+      return (
+        <div className={styles.fieldWrapper}>
+          <Input
+            type={props.type}
+            className={styles.fieldWrapper__input}
+            placeholder={props.placeholder}
+            {...field}
+          />
+          <ErrorMessage component="span" name={field.name} className={styles.fieldError} />
+        </div>
+      );
+    case "select":
+      return (
+        <div className={styles.fieldWrapper}>
+          <Select
+            options={options}
+            type={props.type}
+            className={styles.fieldWrapper__select}
+            {...field}
+          />
+          <ErrorMessage component="span" name={field.name} className={styles.fieldError} />
+        </div>
+      );
+    default:
+      return null;
+  }
+};
