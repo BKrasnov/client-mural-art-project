2d4a068e91dc622bbe9c6e55ff9dcdd8fd7049f3
BKrasnov <bkrasnov235@gmail.com>
2023-01-08 01:23:00 +0700

    Create Select and Input fields M-1

---
+++ src/components/FormikControl/components/Select.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+import { Field, FieldHookConfig } from "formik";
+
+import { OptionsSelect } from "@core/models";
+
+interface SelectProps {
+  options?: OptionsSelect[];
+}
+
+export const Select: FC<FieldHookConfig<string> & SelectProps> = ({ options, ...field }) => {
+  return (
+    <Field
+      component="select"
+      className={field.className}
+      name={field.name}
+      type={field.type}
+      required
+    >
+      {options?.map(option => (
+        <option key={option.key} value={option.value} disabled={option.disabled}>
+          {option.value}
+        </option>
+      ))}
+    </Field>
+  );
+};
