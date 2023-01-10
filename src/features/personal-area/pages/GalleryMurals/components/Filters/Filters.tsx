fb3b53f90df4540910adc5427609b323c25e9c48
BKrasnov <bkrasnov235@gmail.com>
2023-01-10 15:28:22 +0700

    Add formik to Filters M-2

--- src/features/personal-area/pages/GalleryMurals/components/Filters/Filters.tsx
+++ src/features/personal-area/pages/GalleryMurals/components/Filters/Filters.tsx
@@ -4,9 +4,18 @@ import { useAppDispatch, useAppSelector } from "src/store";
 import { selectMuralsListFilters } from "src/store/murals/selectors";
 import { setMuralsFilters } from "src/store/murals/slice";
 
+import { Form, FormikProvider, useFormik } from "formik";
+
+import { FormikControl } from "@components/FormikControl";
+import { UiSubmitButton } from "@components/UI";
+
 import { MuralFilters } from "@core/models";
 
 import styles from "./Filters.module.css";
+
+const initialFormValues: MuralFilters = {
+  searchValue: "",
+};
 
 const FiltersComponent: FC = () => {
   const dispatch = useAppDispatch();
@@ -19,21 +28,18 @@ const FiltersComponent: FC = () => {
     }
   };
 
+  const formik = useFormik({
+    initialValues: initialFormValues,
+    onSubmit: handleSubmit,
+  });
+
   return (
-    <>
-      <div>
-        <form className={styles.wrapperSettings}>
-          <input
-            onChange={text => {
-              handleSubmit({ searchValue: text.target.value });
-            }}
-            type="search"
-            className={styles.search}
-            placeholder="Поиск по названию"
-          />
-        </form>
-      </div>
-    </>
+    <FormikProvider value={formik}>
+      <Form className={styles.filterPanel}>
+        <FormikControl control="search" name="searchValue" type="search" placeholder="Search" />
+        <UiSubmitButton>Submit</UiSubmitButton>
+      </Form>
+    </FormikProvider>
   );
 };
 
